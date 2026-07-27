62a030e622d883f1a10b050d578fdbc6c35972aa
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-07-28 02:39:37 +0530

    Time: 1 ms (77.97%), Space: 54.3 MB (56.36%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+
+    let num = nums.sort((a,b) => b-a)
+    
+    return (num[0] - 1) * (num[1] - 1)
+};
