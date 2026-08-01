f5fefc0271a99122209a4209eb846df5e9da44f5
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-08-01 22:49:15 +0530

    Time: 96 ms (14.94%), Space: 63.6 MB (5.01%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let set = new Set()
+    let left = 0
+    let ans = 0
+
+    for (let i = 0; i < s.length; i++) {
+
+        while (set.has(s[i])) {
+            set.delete(s[left])
+            left++
+        }
+
+        set.add(s[i])
+
+        ans = Math.max(ans, i - left + 1)
+
+    }
+
+    return ans
+};
