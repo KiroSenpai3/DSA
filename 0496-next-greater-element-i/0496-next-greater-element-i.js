1906a8f6f4d3fe3708a1a7c01c1558d25f87ffa2
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-08-09 07:04:26 +0530

    Time: 2 ms (87%), Space: 57.2 MB (64.46%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -5,29 +5,23 @@
  */
 var nextGreaterElement = function(nums1, nums2) {
     let stack = []
-    let arr = []
-    for(let i = nums2.length - 1; i>=0; i--){
-        while(stack.length !== 0 && nums2[i] >= stack[stack.length - 1]){
+    let map = new Map()
+    for(let num of nums2){
+
+        while(num > stack[stack.length - 1] && stack.length){
+            map.set(stack[stack.length - 1] , num)
             stack.pop()
         }
-        if(stack.length == 0){
-                arr.push(-1)
-                stack.push(nums2[i])
-                continue
-            }
-        if(stack.length !== 0){
-            arr.push(stack[stack.length - 1])
-            stack.push(nums2[i])
-        }
+
+        stack.push(num)
     }
-    nums2.reverse()
-    let map = new Map()
-    for(let i = 0; i< nums2.length; i++){
-        map.set(nums2[i], arr[i])
-    }
+
     let ans = []
-    for(let a of nums1){
-        ans.push(map.get(a))
+    
+    for(let num of nums1){
+        if(map.get(num) == undefined) ans.push(-1)
+        else{ans.push(map.get(num))}
     }
+
     return ans
 };
