fad85e9436adea3a6f373701954eb1b2d827abbb
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-08-13 05:50:51 +0530

    Time: 11 ms (91.28%), Space: 66.9 MB (88.03%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
@@ -3,37 +3,35 @@
  * @return {number}
  */
 var largestRectangleArea = function(heights) {
-    let stack = [];
-    let maxArea = 0;
+    let n = heights.length
+    let stack = []
+    let ans = 0
 
-    for(let i = 0; i <= heights.length; i++){
+    for(let i = 0; i <= n; i++){
 
-        let currentHeight = i === heights.length
-            ? 0
-            : heights[i];
+        let left = 0
 
-        while(
-            stack.length &&
-            currentHeight < heights[stack[stack.length - 1]]
-        ){
+        let currentHeight = i === n ? 0 : heights[i]
 
-            let index = stack.pop();
-
-            let height = heights[index];
-
-            let left = stack.length
-                ? stack[stack.length - 1]
-                : -1;
-
-            let width = i - left - 1;
-
-            let area = height * width;
-
-            maxArea = Math.max(maxArea, area);
+        while(stack.length && currentHeight < heights[stack[stack.length - 1]]){
+            let a = stack.pop()
+            if(stack.length){
+                left = stack[stack.length - 1] 
+            }
+            else{
+                left = -1
+            }
+            
+            let width = i - left - 1
+            
+            ans = Math.max(ans, width*heights[a])
+        }
+        
+        if(i < n){
+            stack.push(i)
         }
 
-        stack.push(i);
     }
 
-    return maxArea;
+    return ans
 };
