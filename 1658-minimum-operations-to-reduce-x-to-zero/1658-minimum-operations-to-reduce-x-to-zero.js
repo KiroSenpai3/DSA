90874334eb831fd8eaaa3816a10d88ce8e025950
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-08-20 10:34:48 +0530

    Time: 4 ms (90%), Space: 66.3 MB (76%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} x
+ * @return {number}
+ */
+var minOperations = function(nums, x) {
+    let left = 0
+    let target = 0  
+    let sum = 0
+    let ans = 0
+    for(let right = 0; right < nums.length; right++){
+        sum += nums[right]
+    }
+    target = sum - x
+    sum = 0
+
+    if (target < 0) return -1
+    if (target === 0) return nums.length
+
+    for(let right = 0; right < nums.length; right++){
+        
+        sum+= nums[right]
+        
+        while(sum > target){
+            sum -= nums[left]
+            left++
+        }
+
+        if(sum === target){
+            ans = Math.max(ans, right - left + 1)
+        }
+    }
+
+    return ans? nums.length - ans : -1
+};
