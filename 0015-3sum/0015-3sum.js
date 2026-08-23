fbcffbadb1ec2638f2d9836b58a24e2f447516aa
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-08-23 10:11:06 +0530

    Time: 38 ms (45.28%), Space: 73 MB (28.48%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -4,22 +4,19 @@
  */
 var threeSum = function(nums) {
     let num = nums.sort((a,b) => a-b)
-    let left
-    let right
-    let target
     let ans = []
     for(let i = 0; i < nums.length; i++){
         if(i > 0 && num[i] === num[i-1]) continue
-        target = -num[i]
-        left = i + 1
-        right = num.length - 1 
+        let target = -num[i]
+        let left = i + 1
+        let right = num.length - 1 
         while(left < right){
             if(num[left] + num[right] === target){
                 ans.push([num[i], num[left], num[right]])
                 left++
                 right--
                 while(left < right && num[left] === num[left-1]){ left++}
-            while(left < right && num[right] === num[right+1]){ right--}
+                while(left < right && num[right] === num[right+1]){ right--}
             }
             
             else if(num[left] + num[right] > target) right--
