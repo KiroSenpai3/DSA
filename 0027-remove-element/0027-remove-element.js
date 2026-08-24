3a9b48b08deb51ca49b6e97e88713c4b85436cf6
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-08-24 10:17:21 +0530

    Time: 1 ms (6.37%), Space: 54.6 MB (12.43%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -5,21 +5,11 @@
  */
 var removeElement = function(nums, val) {
     let left = 0
-    let count = 0
-    let right = nums.length - 1
-    while(left <= right){
-        while(right >= 0 && nums[right] === val){
-            right--
-        }
-        while(left < nums.length && nums[left] !== val){
+    for(let right = 0; right < nums.length; right++){
+        if(nums[right] !== val){
+            [nums[left], nums[right]] = [nums[right] , nums[left]]
             left++
         }
-
-        if(left > right) break
-
-        [nums[left] , nums[right]] = [nums[right] , nums[left]]
-        left++
-        right--
     }
-    return right+1
+    return left
 };
