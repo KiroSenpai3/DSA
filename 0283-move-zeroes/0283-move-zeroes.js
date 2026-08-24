24c6ace9cd6688667532419c7e45be79b3536381
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-08-24 09:00:46 +0530

    Time: 4 ms (30.27%), Space: 60.9 MB (19.25%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -4,17 +4,10 @@
  */
 var moveZeroes = function(nums) {
     let left  = 0
-    let right = 0
-    while(left < nums.length){
-        while(left < nums.length && nums[left] !== 0){
+    for(let right = 0; right < nums.length; right++){
+        if(nums[right] !== 0){
+            [nums[left] , nums[right]] = [nums[right] , nums[left]]
             left++
         }
-        right = left
-        while(right < nums.length && nums[right] === 0){
-            right++
-        }
-        if(right === nums.length) break
-        [nums[left], nums[right]] = [nums[right] , nums[left]]
-        left++
     }
 };
