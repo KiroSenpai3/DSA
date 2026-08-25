db7c7678a501c06ae5a41c29534bf19c551f7649
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-08-25 16:41:31 +0530

    Time: 45 ms (94.93%), Space: 57.5 MB (82.54%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let left = 0
+
+    for (let right = 0; right < nums.length; right++) {
+
+        if (left < 2 || nums[right] !== nums[left - 2]) {
+            nums[left] = nums[right]
+            left++
+        }
+    }
+
+    return left
+};
