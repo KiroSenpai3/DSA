a49a700ee66e759a73c7b02c2ff64899c5e0bc65
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-08-26 16:14:56 +0530

    Time: 10 ms (94.46%), Space: 57.2 MB (39.17%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+
+    nums.sort((a,b) => a-b)
+    let ans = Infinity
+
+    for(let i = 0; i < nums.length; i++){
+
+        let left = i + 1
+        let right = nums.length - 1
+
+        while(left < right){
+
+            let sum = nums[i] + nums[left] + nums[right]
+
+            if(Math.abs(target - sum) < Math.abs(target - ans)){
+                ans = sum
+            }
+
+            if(target === sum){
+                return sum
+            }
+            if(sum > target){
+                right--
+            }
+            else left++
+        }
+    }
+
+    return ans
+};
