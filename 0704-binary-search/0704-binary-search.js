81843c60a7e0f64f17b7af0062684326d30ee660
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-08-28 11:24:49 +0530

    Time: 0 ms (100%), Space: 57.6 MB (77.47%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -6,11 +6,11 @@
 var search = function(nums, target) {
     let left = 0
     let right = nums.length - 1
-    while(left<=right){
-        let middle = Math.floor((left + right)/2)
-        if(target === nums[middle]) return middle
-        else if(target > nums[middle]) left = middle + 1
-        else right = middle - 1
+    while(left <= right){
+        let mid = Math.floor((left + right)/2)
+        if(nums[mid] === target) return mid
+        if(nums[mid] < target) left = mid + 1
+        else right = mid - 1
     }
     return -1
 };
