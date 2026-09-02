5c0953f30249eb8476c2fb76a5bc4e23a57b0975
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-09-02 10:59:13 +0530

    Time: 2 ms (13.16%), Space: 55.6 MB (20.98%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let arr = []
+    if(!nums.length) return arr
+    arr[0] = nums[0]
+    for(let i = 1; i < nums.length; i++){
+        arr[i] = nums[i] + arr[i-1]
+    }
+    return arr
+};
