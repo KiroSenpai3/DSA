02b418194cdaef3e07a8b94e70b94e21a682b2f0
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-09-06 22:27:56 +0530

    Time: 1 ms (59.8%), Space: 62.6 MB (10.47%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -5,10 +5,12 @@
 var reverseString = function(s) {
     let recur = function(s, left, right){
         if(left >= right) return
-        recur(s, left+1, right-1)
+        
         let temp = s[left]
         s[left] = s[right]
         s[right] = temp
+
+        recur(s, left+1, right-1)
     }
     recur(s,0, (s.length - 1))
 };
