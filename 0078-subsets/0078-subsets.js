19a1505db8f8e33e5398584bb1ee1a00e738bf7d
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-09-07 21:57:39 +0530

    Time: 1 ms (30.65%), Space: 54.7 MB (69.92%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -6,13 +6,13 @@ var subsets = function(nums) {
     let arr = []
     let recur = function(index, currentSubset){
         if(index === nums.length){
-            arr = [...arr, currentSubset]
+            arr.push(currentSubset)
             return
         }
         
         recur(index + 1, [...currentSubset, nums[index]])
         recur(index + 1, currentSubset)
     }
-    recur(0, arr)
+    recur(0, [])
     return arr
 };
