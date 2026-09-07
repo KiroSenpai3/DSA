d26fcea0624b860e76b264b6f8cafef6273ae391
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-09-07 17:38:01 +0530

    Time: 43 ms (76.49%), Space: 53.9 MB (34.76%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,7 +3,14 @@
  * @return {number}
  */
 var fib = function(n) {
-    if (n==0) return 0
-    if (n==1) return 1
-    return fib(n-1) + fib(n-2)
+    let dp = []
+    const fibo = function(n){
+        if (n==0) return 0
+        if (n==1) return 1
+        if(dp[n] != undefined) return dp[n]
+        let ans = fibo(n-1) + fibo(n-2)
+        dp[n] = ans
+        return ans
+    }
+    return fibo(n)
 };
