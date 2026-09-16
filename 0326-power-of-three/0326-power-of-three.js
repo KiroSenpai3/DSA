26555cd88a634450c414fb94a51d56275eff55dc
Yash Mittal <143866178+KiroSenpai3@users.noreply.github.com>
2026-09-16 20:17:48 +0530

    Time: 7 ms (45.1%), Space: 63.1 MB (98.91%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    let recur = function(n){
+        if(n === 1) return true
+        if(n < 1) return false
+        return recur(n/3)
+    }
+    return recur(n)
+};
